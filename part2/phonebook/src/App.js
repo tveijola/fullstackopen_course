f13115d881534e7ba37170c0db5040a3a8b601ef
tveijola <tommi.veijola@gmail.com>
2020-05-23 15:05:33 +0300

    added styles and notifications to phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Book from './components/Book'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
+import Notification from './components/Notification'
 import personService from './services/persons'
 
 const App = () => {
@@ -10,6 +11,10 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNum, setNewNum] = useState('')
   const [filter, setFilter] = useState('')
+  const [notification, setNotification] = useState({
+    message: null,
+    isError: false
+  })
 
   useEffect(() => {
     personService
@@ -19,50 +24,77 @@ const App = () => {
       })
   }, [])
 
+  const notify = (message, isError) => ({ message, isError })
+
+  const refreshPhonebook = () => {
+    personService
+      .getAll()
+      .then(persons => {
+        setPersons(persons)
+      })
+  }
+
+  const clearForms = () => {
+    setNewName('')
+    setNewNum('')
+  }
+
+  const clearNotification = () => {
+    setNotification({
+      message: null,
+      isError: false
+    })
+  }
+
   const addPerson = (event) => {
     event.preventDefault()
+    let replace = false
     const duplicate = persons.some(person => person.name.toLowerCase() === newName.toLowerCase())
     if (duplicate) {
       console.log('DUPLICATE FOUND')
-      const replace = window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)
-      if (replace) {
-        const id = persons.find(person => person.name.toLowerCase() === newName.toLowerCase()).id
-        const newPerson = {
-          name: newName,
-          number: newNum,
-          id: id
-        }
-        personService
-          .update(newPerson.id, newPerson)
-          .then(returnedPerson =>{
-            setPersons(persons.map(person => person.id !== id ? person : returnedPerson))
-            setNewName('')
-            setNewNum('')
-          })
-      }
-    } else {
-      const newPerson = {
-        name: newName,
-        number: newNum,
-      }
-      personService
-        .create(newPerson)
-        .then(returnedPerson => {
-          setPersons(persons.concat(returnedPerson))
-          setNewName('')
-          setNewNum('')
-        })
+      replace = window.confirm(
+        `${newName} is already added to phonebook, replace the old number with a new one?`
+      )
     }
+
+    if (duplicate && !replace) {
+      return
+    }
+
+    let promise;
+    const newPerson = {
+      name: newName,
+      number: newNum
+    }
+
+    if (replace) {
+      newPerson.id = persons.find(p => p.name.toLowerCase() === newName.toLowerCase()).id
+      promise = personService
+        .update(newPerson.id, newPerson)
+        .then(returnedPerson => setPersons(persons.map(p => p.id !== newPerson.id ? p : returnedPerson)))
+    } else {
+      promise = personService
+        .create(newPerson)
+        .then(returnedPerson => setPersons(persons.concat(returnedPerson)))
+    }
+    
+    promise.then(() => {
+      setNotification(notify(`Added ${newName}`, false))
+      setTimeout(() => clearNotification(), 3000)
+      clearForms()
+    }).catch(error => {
+      setNotification(notify(`Information of ${newName} has been deleted!`, true))
+      setTimeout(() => clearNotification, 5000)
+      refreshPhonebook();
+    })
   }
 
   const deletePerson = (id) => {
-    const confirm = window.confirm(`Delete ${persons.find(person => person.id === id).name}?`)
-    if (confirm){
+    const confirm = window.confirm(`Delete ${persons.find(p => p.id === id).name}?`)
+    if (confirm) {
       personService
-      .remove(id)
-      .then(
-        setPersons(persons.filter(person => person.id !== id))
-      )
+        .remove(id)
+        .then(setPersons(persons.filter(p => p.id !== id)))
     }
   }
 
@@ -72,17 +104,18 @@ const App = () => {
 
   const filteredPersons = filter.length === 0
     ? persons
-    : persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+    : persons.filter(p => p.name.toLowerCase().includes(filter.toLowerCase()))
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification notification={notification} />
       <Filter filter={filter} filterShown={filterShown} />
       <PersonForm
         newName={newName} handleNameChange={handleNameChange}
         newNum={newNum} handleNumChange={handleNumChange} addPerson={addPerson} />
-      <Book persons={filteredPersons} 
-            del={deletePerson} />
+      <Book persons={filteredPersons}
+        del={deletePerson} />
     </div>
   )
 }
---
+++ part2/phonebook/src/components/Notification.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Notification = ({notification}) => {
+  if (notification.message === null) {
+    return null
+  }
+  const type = (notification.isError) ? "error" : "notification"
+  return (
+    <div className={type}>Error! {notification.message}</div>
+  )
+}
+
+export default Notification
