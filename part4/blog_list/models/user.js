2c205d68f1ae00894db4f120321df69305058259
tveijola <tommi.veijola@gmail.com>
2020-07-09 18:45:35 +0300

    implemented adding and getting users

---
+++ part4/blog_list/models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+mongoose.set('useFindAndModify', false)
+
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  passwordHash: String
+})
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.passwordHash
+  }
+})
+
+module.exports = mongoose.model('User', userSchema)
