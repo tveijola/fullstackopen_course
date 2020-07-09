bb56eef31ab4604af8a02bb7a3f6a1ed69ce22f2
tveijola <tommi.veijola@gmail.com>
2020-07-09 20:29:24 +0300

    configured mongoose to useCreateIndex to get rid of deprecation warning

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -11,7 +11,7 @@ const mongoose = require('mongoose')
 
 logger.info('connecting to', config.MONGODB_URI)
 
-mongoose.connect(config.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then(() => logger.info('connected to MongoDB'))
   .catch((error) => logger.error('error connecting to MongoDB:', error.message))
 
