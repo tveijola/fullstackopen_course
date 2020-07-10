c8c61e46fd6b0d21c5ce76fb64faf2bbb0ecfc39
tveijola <tommi.veijola@gmail.com>
2020-07-10 14:58:19 +0300

    implemented token based authentication

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
@@ -21,6 +22,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.errorHandler)
 
