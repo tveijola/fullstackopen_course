f7b281bd5b82e8703396e61438c20088a36b05f4
tveijola <tommi.veijola@gmail.com>
2020-07-11 16:37:25 +0300

    implemented show details button for blogs

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,31 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails)
+  }
+
+  const details = (blog) => {
+    return (
+      <div>
+        {blog.url}<br/>
+        Likes: {blog.likes}<button style={{ marginLeft: 5 }}>Like</button><br/>
+        {blog.creator.name}<br/>
+      </div>
+    )
+  }
+
+  return (
+    <div className="blog">
+      {blog.title} {blog.author}
+      <button style={{ marginLeft: 5 }} onClick={toggleDetails}>
+        {showDetails ? 'Hide Details' : 'View Details'}
+      </button>
+      {showDetails && details(blog)}
+    </div>
+  )
+}
 
 export default Blog
