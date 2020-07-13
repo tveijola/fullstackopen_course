44f23a0eb5346fecb1a4078bff89756b64fdfb02
tveijola <tommi.veijola@gmail.com>
2020-07-13 19:53:43 +0300

    small change to logging

--- part4/blog_list/utils/logger.js
+++ part4/blog_list/utils/logger.js
@@ -1,11 +1,15 @@
 const info = (...params) => {
   if (process.env.NODE_ENV !== 'test') {
     console.log(...params)
+  } else {
+    console.log('TEST-ENV ---', ...params)
   }
 }
 const error = (...params) => {
   if (process.env.NODE_ENV !== 'test') {
     console.error(...params)
+  } else {
+    console.error('TEST-ENV ---', ...params)
   }
 }
 
