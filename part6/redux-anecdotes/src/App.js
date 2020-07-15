bf78b2b3f9ea0e3bac3e386a5d93b21af5563ba0
tveijola <tommi.veijola@gmail.com>
2020-07-15 10:44:15 +0300

    separated anecdotelist into its own component

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,33 +1,11 @@
 import React from 'react'
-import { incrementVotes } from './reducers/anecdoteReducer'
-import { useSelector, useDispatch } from 'react-redux'
 import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  anecdotes.sort((anec1, anec2) => anec2.votes - anec1.votes)
-
-  const dispatch = useDispatch()
-
-  const vote = (id) => {
-    console.log('vote', id)
-    dispatch(incrementVotes(id))
-  }
-
   return (
     <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes} votes
-            <button style={{ marginLeft: 5 }} onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteList />
       <AnecdoteForm />
     </div>
   )
---
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { incrementVotes } from '../reducers/anecdoteReducer'
+import { useDispatch, useSelector } from 'react-redux'
+
+const AnecdoteList = () => {
+  const dispatch = useDispatch()
+  const anecdotes = useSelector(state => state)
+  anecdotes.sort((anec1, anec2) => anec2.votes - anec1.votes)
+
+  const vote = (id) => {
+    console.log('vote', id)
+    dispatch(incrementVotes(id))
+  }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes} votes
+            <button style={{ marginLeft: 5 }} onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default AnecdoteList
