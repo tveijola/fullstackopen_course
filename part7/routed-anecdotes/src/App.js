121576c3dee1a6320eb8d6af9af75743537ba266
tveijola <tommi.veijola@gmail.com>
2020-07-21 18:31:43 +0300

    simplified anecdote creation with useField hook

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -4,6 +4,7 @@ import {
 } from 'react-router-dom'
 
 import React, { useState } from 'react'
+import { useField } from './hooks'
 
 const Menu = () => {
   const padding = {
@@ -59,7 +60,7 @@ const Anecdote = ({ anecdotes }) => {
     )
   }
   return (
-    <Redirect to="/"/>
+    <Redirect to="/" />
   )
 }
 
@@ -86,17 +87,17 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
   const history = useHistory()
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     history.push('/')
@@ -108,15 +109,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
@@ -148,7 +149,7 @@ const App = () => {
   const notify = (message) => {
     setNotification(message)
     setTimeout(() => setNotification(''), 10000)
-  } 
+  }
 
   const addNew = (anecdote) => {
     anecdote.id = (Math.random() * 10000).toFixed(0)
