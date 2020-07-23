d37a21e0f226078230f1d52cb6c78782142bab5d
tveijola <tommi.veijola@gmail.com>
2020-07-23 21:35:02 +0300

    added notifications to blog actions

--- part7/bloglist/bloglist-frontend/src/reducers/blogReducer.js
+++ part7/bloglist/bloglist-frontend/src/reducers/blogReducer.js
@@ -1,4 +1,11 @@
 import blogService from '../services/blogs'
+import { setNotification, clearNotification } from './notificationReducer'
+
+const notify = (dispatch, message, isError) => {
+  dispatch(setNotification(message, isError))
+  setTimeout(() => dispatch(clearNotification()), (isError) ? 3000 : 2000)
+}
+
 const blogReducer = (state = [], action) => {
   switch (action.type) {
     case 'CREATE_BLOG':
@@ -21,11 +28,16 @@ const blogReducer = (state = [], action) => {
 
 export const createBlog = (blogObject) => {
   return async dispatch => {
-    const response = await blogService.create(blogObject)
-    dispatch({
-      type: 'CREATE_BLOG',
-      data: response
-    })
+    try {
+      const response = await blogService.create(blogObject)
+      dispatch({
+        type: 'CREATE_BLOG',
+        data: response
+      })
+      notify(dispatch, `Created blog: ${response.title} ${response.author}`, false)
+    } catch (exception) {
+      notify(dispatch, 'New blog must contain at least title or url', true)
+    }
   }
 }
 
@@ -41,28 +53,36 @@ export const fetchBlogs = () => {
 
 export const likeBlog = (blogObject) => {
   return async dispatch => {
-    const modifiedBlog = {
-      ...blogObject,
-      creator: blogObject.creator.id,
-      likes: blogObject.likes + 1,
+    try {
+      const modifiedBlog = {
+        ...blogObject,
+        creator: blogObject.creator.id,
+        likes: blogObject.likes + 1,
+      }
+      const savedBlog = await blogService.put(modifiedBlog)
+      dispatch({
+        type: 'INCREMENT_LIKES',
+        data: savedBlog
+      })
+    } catch (exception) {
+      notify(dispatch, exception.message, true)
     }
-    const savedBlog = await blogService.put(modifiedBlog)
-    dispatch({
-      type: 'INCREMENT_LIKES',
-      data: savedBlog
-    })
   }
 }
 
 export const removeBlog = (blogObject) => {
   return async dispatch => {
-    const result = window.confirm(`Remove blog ${blogObject.title} by ${blogObject.author}`)
-    if (result) {
-      await blogService.remove(blogObject)
-      dispatch({
-        type: 'REMOVE_BLOG',
-        data: blogObject.id
-      })
+    try {
+      const result = window.confirm(`Remove blog ${blogObject.title} by ${blogObject.author}`)
+      if (result) {
+        await blogService.remove(blogObject)
+        dispatch({
+          type: 'REMOVE_BLOG',
+          data: blogObject.id
+        })
+      }
+    } catch (exception) {
+      notify(dispatch, exception.message, true)
     }
   }
 }
