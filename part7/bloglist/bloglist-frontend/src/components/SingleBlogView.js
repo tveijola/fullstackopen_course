9d688f68c3d7f66707994b4a1ef04757521b3ce2
tveijola <tommi.veijola@gmail.com>
2020-07-24 14:48:26 +0300

    added individual blog view

---
+++ part7/bloglist/bloglist-frontend/src/components/SingleBlogView.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { likeBlog } from '../reducers/blogReducer'
+import { useDispatch } from 'react-redux'
+
+const SingleBlogView = ({ blog }) => {
+
+  const dispatch = useDispatch()
+
+  if (!blog) {
+    return null
+  }
+
+  return (
+    <div>
+      <h2>{blog.title}</h2>
+      <div>{blog.url}</div>
+      <div>
+        {blog.likes} likes
+        <button style={{ marginLeft: 5 }} onClick={() => dispatch(likeBlog(blog))}>
+          Like
+        </button>
+      </div>
+      <div>Added by {blog.creator.name}</div>
+    </div>
+  )
+}
+
+export default SingleBlogView
