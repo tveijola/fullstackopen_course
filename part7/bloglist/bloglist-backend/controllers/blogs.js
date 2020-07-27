89b862348482beec9fabec4679907df105c587a4
tveijola <tommi.veijola@gmail.com>
2020-07-27 19:31:11 +0300

    added comment functionality to blogs in the backend

--- part7/bloglist/bloglist-backend/controllers/blogs.js
+++ part7/bloglist/bloglist-backend/controllers/blogs.js
@@ -2,6 +2,7 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
+const blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -75,6 +76,20 @@ blogsRouter.put('/:id', async (request, response) => {
   }
   const updatedBlog = await Blog
     .findByIdAndUpdate(request.params.id, blog, { new: true })
+    .populate('creator', { username: 1, name: 1, id: 1 })
+
+  response.json(updatedBlog.toJSON())
+})
+
+blogsRouter.post('/:id/comments', async (request, response) => {
+  const body = request.body
+  const blogToComment = await Blog.findById(request.params.id)
+  blogToComment.comments = [...blogToComment.comments, body.comment]
+  console.log({
+    ...blogToComment
+  })
+  const updatedBlog = await Blog
+    .findByIdAndUpdate(request.params.id, blogToComment, { new: true })
     .populate('creator', { username: 1, name: 1, id: 1 })
 
   response.json(updatedBlog.toJSON())
