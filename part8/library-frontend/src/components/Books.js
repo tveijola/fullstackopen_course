e4500c3209ed87286f689707df1076df4333356a
tveijola <tommi.veijola@gmail.com>
2020-08-05 21:26:13 +0300

    added filtering book list by genre

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,14 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Books = (props) => {
+
+  const [filterGenre, setFilterGenre] = useState(null)
+
   if (!props.show) {
     return null
+  }
+
+  const allGenres = []
+  props.books.forEach(book => {
+    book.genres.forEach(genre => {
+      if (!allGenres.includes(genre)) {
+        allGenres.push(genre)
+      }
+    })
+  })
+
+  const filteredBooks = filterGenre
+    ? props.books.filter(book => {
+      return book.genres.some(genre => filterGenre.includes(genre))
+    })
+    : props.books
+
+  const filterMessage = () => {
+    return (
+      <div>
+        Showing books in genre <b>{filterGenre}</b>
+        <button style={{ margin: 15 }} onClick={() => setFilterGenre(null)}>Clear filter</button>
+      </div>
+    )
   }
 
   return (
     <div>
       <h2>books</h2>
-
+      {filterGenre ? filterMessage() : null}
       <table>
         <tbody>
           <tr>
@@ -20,7 +47,7 @@ const Books = (props) => {
               published
             </th>
           </tr>
-          {props.books.map(a =>
+          {filteredBooks.map(a =>
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -29,6 +56,16 @@ const Books = (props) => {
           )}
         </tbody>
       </table>
+      <br />
+      filter by
+      <br />
+      {allGenres.map(genre =>
+        <button
+          key={genre}
+          onClick={() => setFilterGenre(genre)}>
+          {genre}
+        </button>
+      )}
     </div>
   )
 }
